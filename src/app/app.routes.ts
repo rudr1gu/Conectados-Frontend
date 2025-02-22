2ed671fd2863309bac89d0f462619d6e941dfbf6
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-22 20:46:04 -0300

    :art: Refactor layout structure and add login and registration components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'cadastro', component: CadastroComponent},
+];
